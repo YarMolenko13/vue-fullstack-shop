3e814376a9c8ac8d2be2120cf0386db0c35fbfd6
YarMolenko13 <blistacelniy@yahoo.com>
2021-03-22 19:13:57 +0300

    push github pages

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,5 @@
 // vue.config.js
 module.exports = {
-    // // https://cli.vuejs.org/config/#devserver-proxy
-    // devServer: {
-    //     port: 3000,
-    //     proxy: {
-    //         '/': {
-    //             target: 'https://full-stack-shop-api.herokuapp.com',
-    //             ws: true,
-    //             changeOrigin: true
-    //         }
-    //     }
-    // },
     css: {
         loaderOptions: {
             sass: {
